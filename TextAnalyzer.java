e0d5c5d84ea8505b23a0f90995d798a9212fc0ef
Javier Palomares <javier.palomares.90@gmail.com>
2019-10-22 19:25:41 -0700

    Text Analyzer finally compiles

--- TextAnalyzer.java
+++ TextAnalyzer.java
@@ -1,22 +1,42 @@
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.join.TupleWritable;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-// Do not change the signature of this class
-public class TextAnalyzer extends Configured implements Tool {
 
+public class TextAnalyzer extends Configured implements Tool
+{
     // Replace "?" with your own output key / value types
     // The four template data types are:
-    //     <Input Key Type, Input Value Type, Output Key Type, Output Value Type>
-    public static class TextMapper extends Mapper<LongWritable, Text, ?, ?> {
+    /**
+     * TODO: Check these output types. They may not be correct. 
+     * For now, set them to Text just for compiliation purposes
+     */
+    //   <Input Key Type, Input Value Type, Output Key Type, Output Value Type>
+    public static class TextMapper extends Mapper<LongWritable, Text, Text, Text> {
         public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException
         {
@@ -26,8 +46,8 @@ public class TextAnalyzer extends Configured implements Tool {
 
     // Replace "?" with your own key / value types
     // NOTE: combiner's output key / value types have to be the same as those of mapper
-    public static class TextCombiner extends Reducer<?, ?, ?, ?> {
-        public void reduce(Text key, Iterable<Tuple> tuples, Context context)
+    public static class TextCombiner extends Reducer<LongWritable, Text, Text, Text> {
+        public void reduce(Text key, Iterable<TupleWritable> tuples, Context context)
             throws IOException, InterruptedException
         {
             // Implementation of you combiner function
@@ -36,13 +56,14 @@ public class TextAnalyzer extends Configured implements Tool {
 
     // Replace "?" with your own input key / value types, i.e., the output
     // key / value types of your mapper function
-    public static class TextReducer extends Reducer<?, ?, Text, Text> {
+    public static class TextReducer extends Reducer<Text, Text, Text, Text> {
         private final static Text emptyText = new Text("");
 
-        public void reduce(Text key, Iterable<Tuple> queryTuples, Context context)
+        public void reduce(Text key, Iterable<TupleWritable> queryTuples, Context context)
             throws IOException, InterruptedException
         {
             // Implementation of you reducer function
+           Map<String,Integer> map = null;
 
             // Write out the results; you may change the following example
             // code to fit with your reducer function.
@@ -67,11 +88,11 @@ public class TextAnalyzer extends Configured implements Tool {
 
         // Setup MapReduce job
         job.setMapperClass(TextMapper.class);
-        
+
         // set local combiner class
         job.setCombinerClass(TextCombiner.class);
 
-        // set reducer class        
+        // set reducer class
         job.setReducerClass(TextReducer.class);
 
         // Specify key / value types (Don't change them for the purpose of this assignment)
@@ -94,64 +115,4 @@ public class TextAnalyzer extends Configured implements Tool {
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
-    // Do not modify the main method
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new TextAnalyzer(), args);
-        System.exit(res);
-    }
-
-    // Class to represent the nodes in the graph. Each node represent a word in the file
-    public static class Node
-    {
-        private String word;
-
-        public Node(String word)
-        {
-            this.setWord(word);
-        }
-
-        public Node()
-        {
-        }
-
-        public void setWord(String word)
-        {
-            this.word = word;
-        }
-
-        public String getWord()
-        {
-            return this.word;
-        }
-    }
-
-    // Class to represent the edges in the nodes.
-    public static class Edge
-    {
-        private Node u;
-        private Node v;
-        private int weight;
-
-        public Edge(Node n1,Node n2, int weight)
-        {
-            this.u = n1;
-            this.v = n2;
-            this.weight = weight;
-        }
-
-        public int getWeight()
-        {
-            return this.weight;
-        }
-
-        public void setWeight(int weight)
-        {
-            this.weight = weight;
-        }
-
-    }
-
 }
-
-
-
